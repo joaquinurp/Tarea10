c35ba619d64f19f12a503af2b48abf0c680a7297
Joaquin <joaquinurp@gmail.com>
2016-05-27 21:01:25 +0200

    elimino la carpeta Tarea10 (añadida por error)

--- ExamenJunio2013Prob1/src/TestTablon/TATest.java
+++ ExamenJunio2013Prob1/src/TestTablon/TATest.java
@@ -2,7 +2,14 @@ package TestTablon;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+
+import junio2013.AnuncianteNoExisteException;
+import junio2013.Anuncio;
+import junio2013.IBaseDeDatosDeAnunciantes;
+import junio2013.IBaseDeDatosDePagos;
+import junio2013.TablonDeAnuncios;
 
 public class TATest {
 
