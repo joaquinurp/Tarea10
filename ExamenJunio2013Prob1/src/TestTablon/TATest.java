97c3ea185c60bd5e301d65717d1f011efc69ba7b
Joaquin <joaquinurp@gmail.com>
2016-05-27 20:33:16 +0200

    Primer commit

---
+++ ExamenJunio2013Prob1/src/TestTablon/TATest.java
@@ -0,0 +1,14 @@
+package TestTablon;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TATest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
