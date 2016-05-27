67e6b1b46b77cff016e21c420b72e9c228b5da54
Joaquin <joaquinurp@gmail.com>
2016-05-27 21:55:07 +0200

    Test2 añadido al codigo

--- ExamenJunio2013Prob1/src/TestTablon/TATest.java
+++ ExamenJunio2013Prob1/src/TestTablon/TATest.java
@@ -22,5 +22,16 @@ public class TATest {
 		TablonDeAnuncios tablon = new TablonDeAnuncios();
 		assertEquals(1,tablon.anunciosPublicados());
 	}
+	
+	@Test
+	public void Test2AnuncioLAEMPRESAComprobarAumenta() {
+		
+		TablonDeAnuncios tablon = new TablonDeAnuncios();
+		Anuncio adEmpresa = new Anuncio("", "", "LA EMPRESA");
+		int anuncios = tablon.anunciosPublicados();
+		tablon.publicarAnuncio(adEmpresa, IBDAnunciantes, IBDPagos);
+		assertEquals(anuncios+1, tablon.anunciosPublicados());
+		
+	}
 
 }
